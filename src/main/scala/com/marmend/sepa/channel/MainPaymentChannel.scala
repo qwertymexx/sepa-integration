61f19eee4c82d887137ecba9900663e33392e2f5
Maksym Panov <mexx@marmend.com>
2017-08-28 17:42:52 +0200

    refactoring

--- src/main/scala/com/marmend/sepa/channel/MainPaymentChannel.scala
+++ src/main/scala/com/marmend/sepa/channel/MainPaymentChannel.scala
@@ -1,18 +1,39 @@
 package com.marmend.sepa.channel
 
-import com.marmend.sepa.channel.file.Pain001
+import com.marmend.sepa.channel.Model.MyBankAccount
+
 
 object MainPaymentChannel {
 
   /**
     * The service generate SEPA valid payload to proceed with SEPA protocols.
     */
-  def main (args: Array[String]) {
+  def main(args: Array[String]) {
 
-    val pc = new PaymentExecutor(MyBankAccount, Pain001)
 
-    pc.execute()
+    val transList = List(
+      """
+     {
+      "endToEndId": "45935",
+      "instdAmt": 234.29,
+      "iBAN": "DE12300800000000001234",
+      "rmtInf": "Kundennummer 1234567 Rechnungsnummer 1111 purchase",
+      "fullName": "John Doe"
+     }
+    """,
+      """
+    {
+      "endToEndId": "12345",
+      "instdAmt": 234.99,
+      "iBAN": "DE12300800000000001234",
+      "rmtInf": "Test",
+      "fullName": "Jon Doe brother"
+    }
+    """)
 
+   val result = new PaymentExecutor(MyBankAccount, transList).executeCreditTransfer()
+
+    print(result.fullXmlFile())
   }
 
 
--- src/main/scala/com/marmend/sepa/channel/PaymentExecutor.scala
+++ src/main/scala/com/marmend/sepa/channel/PaymentExecutor.scala
@@ -1,58 +1,32 @@
 package com.marmend.sepa.channel
-import com.marmend.sepa.channel.file.transaction.ISO20022Transaction
-import com.marmend.sepa.channel.file.ISO20022
+
+import com.marmend.sepa.channel.Model.{Account, Pain001, Pain001Transaction, Transaction}
+import org.json4s.DefaultFormats
+import org.json4s.JsonAST.JValue
+import org.json4s.native.JsonMethods.parse
 
 /**
-  * @param account: Your Company account
-  * @param isoFileFormat: The requested File Format
-  * This class opens a payment com.marmend.sepa.channel.
-  * The com.marmend.sepa.channel takes all messages from one message queue, creates an ISO20022 file from it and
-  * passes the file to the next message queue.
-  * Each com.marmend.sepa.channel is bound to one of your Company Account and one File format (pain001 | pain008).
-  * This applies also to the message queue the com.marmend.sepa.channel reads from and writes to.
+  * @param account : Your Company account
+  *                This class opens a payment com.marmend.sepa.channel.
+  *                The com.marmend.sepa.channel takes all messages from one message queue, creates an ISO20022 file from it and
+  *                passes the file to the next message queue.
+  *                Each com.marmend.sepa.channel is bound to one of your Company Account and one File format (pain001 | pain008).
+  *                This applies also to the message queue the com.marmend.sepa.channel reads from and writes to.
   */
-class PaymentExecutor(account : Account, isoFileFormat : ISO20022) {
+class PaymentExecutor(account: Account, transaction: List[String]) {
 
-  val json = List(
-    """ 
-     {
-      "endToEndId": "45935",
-      "instdAmt": 234.29,
-      "iBAN": "DE12300800000000001234",
-      "rmtInf": "Kundennummer 1234567 Rechnungsnummer 1111 purchase",
-      "fullName": "John Doe"
-     }
-    """,
-    """ 
-    {
-      "endToEndId": "12345", 
-      "instdAmt": 234.99,
-      "iBAN": "DE12300800000000001234",
-      "rmtInf": "Test",
-      "fullName": "Jon Doe brother"
-    }
-    """)
 
-  /**
-    * Takes all messages from the queue the PaymentChannel is bound to.
-    * Transform the messages into ISO payment information.
-    * Then assembles the ISO Payment File and sends it to the next message queue.
-    */
-  def execute() {
+  private def jsonObject(jsonString: String): JValue = {
+    parse(jsonString)
+  }
 
-    //The request to SEPA credit transfer (could be an array of transactions)
-    //so far mock with json above
-    val request = json
+  def executeCreditTransfer(): Pain001 = {
 
-    val parsedTransfers = request.map { x => ISO20022Transaction.apply(isoFileFormat, x) }.flatten
+    implicit val formats = DefaultFormats
 
-    //assemble File from xml transactions
-    val xml = ISO20022(isoFileFormat, account, parsedTransfers)
-
-    // send file to SEPA broker, so far just println
-    xml.map { x => println(x.fullXmlFile()) }
-
-    //ack original messsages 
+      Pain001(account, transaction.map(
+        json => Pain001Transaction(jsonObject(json).extract[Transaction])
+      ))
 
   }
 
